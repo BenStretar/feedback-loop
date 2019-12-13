6577ae1d2795c09441290373e21d4c36bf4332a1
Ben Stretar <ben.stretar@gmail.com>
2019-12-13 16:40:38 -0600

    support & comments added to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
-import FeelingForm from '../FeelingForm/FeelingForm';
+import { HashRouter as Router, Route } from 'react-router-dom'
+import Feeling from '../Feeling/Feeling';
+import Understanding from '../Understanding/Understanding';
+import Support from '../Support/Support';
+import Comments from '../Comments/Comments';
 
 class App extends Component {
   render() {
@@ -12,7 +16,14 @@ class App extends Component {
           <h4><i>Don't forget it!</i></h4>
         </header>
         <br/>
-        <FeelingForm/>
+        <Router>
+          <form>
+            <Route exact path="/" component={Feeling} />
+            <Route exact path="/understanding" component={Understanding} />
+            <Route exact path="/support" component={Support} />
+            <Route exact path="/comments" component={Comments} />
+          </form>
+        </Router>
       </div>
     );
   }
