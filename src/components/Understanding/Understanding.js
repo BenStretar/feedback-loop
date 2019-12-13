d5a5998c25af1e446b7378425a33ad24f72bd02e
Ben Stretar <ben.stretar@gmail.com>
2019-12-13 16:32:02 -0600

    comp for understanding input

---
+++ src/components/Understanding/Understanding.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class understanding extends Component{
+    render(){
+        return(
+            <>
+                <label>How well are you understanding the content? </label><br/>
+                <input type="number" placeholder="" />
+
+                <br />
+            </>
+        )
+    }
+}
+
+export default understanding;
