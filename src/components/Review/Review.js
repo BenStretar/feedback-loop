fd069efed4eede71a1bfa5dd1cf7255c65f9a843
Ben Stretar <ben.stretar@gmail.com>
2019-12-13 18:22:44 -0600

    added h2 & button into Review

---
+++ src/components/Review/Review.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Review extends Component{
+
+    render(){
+        return(
+            <>
+                <h2>Review Your Feedback</h2>
+                <button>Submit</button>
+            </>
+        )
+    }
+}
+
+export default Review;
