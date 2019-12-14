151a6151a7116541e0a5e7b60a777f15739027d9
Ben Stretar <ben.stretar@gmail.com>
2019-12-13 18:38:15 -0600

    redux imported & logger imported

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Redux Imports
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import {Provider} from 'react-redux'
+
+
+const storeInstance = createStore(
+    combineReducers({
+
+    }),
+    applyMiddleware(logger)
+)
+
+ReactDOM.render(<Provider stort={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
