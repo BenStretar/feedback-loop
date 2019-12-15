048a804293c27e33b1314bc31f7ed0e47d35f4ee
Ben Stretar <ben.stretar@gmail.com>
2019-12-15 10:23:04 -0600

    added reducers for components

--- src/index.js
+++ src/index.js
@@ -9,13 +9,51 @@ import logger from 'redux-logger';
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import {Provider} from 'react-redux'
 
+const reviewItems = (state = [], action) => {
+    if(action.type === `GET_REVIEW`) {
+        return action.payload;
+    }
+    return state;
+}
 
-const storeInstance = createStore(
+const feelingReducer = (state = [], action) => {
+    if(action.type === 'FEELING') {
+        return action.payload;
+    }
+    return state;
+}
+
+const understandingReducer = (state = [], action) => {
+    if(action.type === 'UNDERSTANDING') {
+        return action.payload;
+    }
+    return state;
+}
+
+const supportReducer = (state = [], action) => {
+    if(action.type === 'SUPPORT') {
+        return action.payload;
+    }
+    return state;
+}
+
+const commentsReducer = (state =[], action) => {
+    if(action.type === 'COMMENT') {
+        return action.payload;
+    }
+    return state
+}
+
+const reduxStore = createStore(
     combineReducers({
-
+        reviewItems,
+        feelingReducer,
+        understandingReducer,
+        supportReducer,
+        commentsReducer
     }),
     applyMiddleware(logger)
 )
 
-ReactDOM.render(<Provider stort={storeInstance}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={reduxStore}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
