17a60d6bea0f574d3468a4fd5e77f93ab71a4d92
Ben Stretar <ben.stretar@gmail.com>
2019-12-15 12:05:26 -0600

    imports and state added, dispatch comment

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 
 class Feeling extends Component{
+
+    state={
+        aComment: ''
+    }
+
+    handleImport=(event)=>{
+        this.setState({
+            aComment: event.target.value
+        })
+    }
 
     handleNext=()=>{
         console.log('clickedComments')
         this.props.history.push('/review');
+        this.props.dispatch({type: 'COMMENT', payload: this.state.aComment})
     }
 
     render(){
         return(
             <>
                 <h2>Any comments you want to leave? </h2>
-                <input type="text" placeholder="" />
+                <input type="text" placeholder="" onChange={this.handleImport}/>
                 <button onClick={this.handleNext}>Next</button>
             </>
         )
     }
 }
 
-export default Feeling;
+export default withRouter(connect()(Feeling));
