cdb5536b1706e2e4c6471700f707e12f56994164
Ben Stretar <ben.stretar@gmail.com>
2019-12-15 12:05:45 -0600

    imports and state added, dispatch support

--- src/components/Support/Support.js
+++ src/components/Support/Support.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 
 class Feeling extends Component{
+
+    state={
+        aSupport: ''
+    }
+
+    handleInput=(event)=>{
+        this.setState({
+            aSupport: event.target.value
+        })
+    }
 
     handleNext=()=>{
         console.log('clickedSupport')
         this.props.history.push('/comments');
+        this.props.dispatch({type: 'SUPPORT', payload: this.state.aSupport})
     }
 
     render(){
         return(
             <>
                 <h2>How well are you being supported? </h2>
-                <input type="number" placeholder="" max="10"/>
+                <input type="number" placeholder="" max="10" onChange={this.handleInput}/>
                 <button onClick={this.handleNext}>Next</button>
             </>
         )
     }
 }
 
-export default Feeling;
+export default withRouter(connect()(Feeling));
