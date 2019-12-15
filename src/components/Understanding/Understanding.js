8604c8fe00264a794ec2eb3da81cc639257c4c6f
Ben Stretar <ben.stretar@gmail.com>
2019-12-15 12:06:52 -0600

    imports and state added, dispatch understanding

--- src/components/Understanding/Understanding.js
+++ src/components/Understanding/Understanding.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 
 class understanding extends Component{
+
+    state = {
+        aUnderstanding: ''
+    }
+
+    handleInput=(event)=>{
+        this.setState({
+            aUnderstanding: event.target.value
+        })
+    }
 
     handleNext=()=>{
         console.log('clickedUnderstanding')
         this.props.history.push('/support');
+        this.props.dispatch({type: 'UNDERSTANDING', payload: this.state.aUnderstanding})
     }
 
     render(){
         return(
             <>
                 <h2>How well are you understanding the content? </h2>
-                <input type="number" placeholder="" max="10"/>
+                <input type="number" placeholder="" max="10" onChange={this.handleInput}/>
                 <button onClick={this.handleNext}>Next</button>
             </>
         )
     }
 }
 
-export default understanding;
+export default withRouter(connect()(understanding));
