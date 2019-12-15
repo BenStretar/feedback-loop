37ca1618efa89e5aeef2f94308bcea6f72935db9
Ben Stretar <ben.stretar@gmail.com>
2019-12-15 12:05:15 -0600

    imports and state added, dispatch feeling

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 
 class Feeling extends Component{
 
+    state={
+        aFeeling: ''
+    }
+
+    handleInput=(event)=>{
+        this.setState({
+            aFeeling: event.target.value
+        })        
+    }
+
+    //button
     handleNext=()=>{
         console.log('clickedFeelings')
         this.props.history.push('/understanding');
+        this.props.dispatch({type: 'FEELING', payload: this.state.aFeeling})
     }
 
     render(){
         return(
             <>
                 <h2>How are you feeling?</h2>
-                <input type="number" placeholder="" max="10"/>
+                <input type="number" placeholder="" max="10" onChange={this.handleInput}/>
                 <button onClick={this.handleNext}>Next</button>
             </>
         )
     }
 }
 
-export default Feeling;
+export default withRouter(connect()(Feeling));
