74c4664d8ca5eaa29e77c684e0a217ce4419a532
Ben Stretar <ben.stretar@gmail.com>
2019-12-15 21:53:20 -0600

    removed withRouter

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
+//import { withRouter } from 'react-router-dom'
 
 class Feeling extends Component{
 
@@ -34,4 +34,4 @@ class Feeling extends Component{
     }
 }
 
-export default withRouter(connect()(Feeling));
+export default connect()(Feeling);
--- src/components/Understanding/Understanding.js
+++ src/components/Understanding/Understanding.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
+//import { withRouter } from 'react-router-dom'
 
 class understanding extends Component{
 
@@ -34,4 +34,4 @@ class understanding extends Component{
     }
 }
 
-export default withRouter(connect()(understanding));
+export default connect()(understanding);
