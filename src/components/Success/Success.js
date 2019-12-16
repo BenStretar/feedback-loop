e2f5e45c62a9a04aa1d3cf41910fb23867a3f781
Ben Stretar <ben.stretar@gmail.com>
2019-12-16 06:38:14 -0600

    added text and button to go back to the start

--- src/components/Success/Success.js
+++ src/components/Success/Success.js
@@ -3,10 +3,16 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 
 class Success extends Component{
+    goToFeel = () => (
+        this.props.history.push('/') // Move user back to the start
+    )
+
     render(){
         return(
             <>
-            <p>Hello There</p>
+            <p>Success!</p>
+            <p>Thank you for adding feedback!</p>
+            <button onClick={this.goToFeel}>Submit new feedback</button>
             </>
         )
     }
