1112969a9d5c81d3144b6712220b5325f0dda27c
Ben Stretar <ben.stretar@gmail.com>
2019-12-16 06:45:31 -0600

    import axios, trying to get review to work

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
+//import { withRouter } from 'react-router-dom'
+import Axios from 'axios';
 
 class Review extends Component{
 
+    //Takes user to next page
+    postReview = () => {
+        let feedback = {
+            feeling: this.props.reduxState.feelingReducer.feeling,
+            understanding: this.props.reduxState.feelingReducer.understanding,
+            support: this.props.reduxState.feelingReducer.support,
+            comments: this.props.reduxState.feelingReducer.comment
+        }
+
+        Axios.post('/feedback', feedback)
+            .then(response => {
+                console.log(response)
+            this.props.history.push('/success'); 
+        }).catch(error => {
+            console.log('Error adding feedback', error)
+            alert('Error adding feedback. See the console for details')
+        })
+    }
+
     render(){
+        console.log(this.props.reduxState)
         return(
             <>
+            {JSON.stringify(this.props.reduxState)}
                 <h2>Review Your Feedback</h2>
-                <p>Feeling: </p>
-                <p>Understanding: </p>
-                <p>Support: </p>
-                <p>Comments: </p>
-                <button>Submit</button>
+                <p>Feeling: {this.props.reduxState.feelingReducer.feeling}</p>
+                <p>Understanding: {this.props.reduxState.feelingReducer.understanding}</p>
+                <p>Support: {this.props.reduxState.feelingReducer.support}</p>
+                <p>Comments: {this.props.reduxState.feelingReducer.comment}</p>
+                <button onClick={this.postReview}>Submit</button> {/* takes user to Success */}
             </>
         )
     }
 }
 
-export default withRouter(connect()(Review));
+const putPropsOnRedux = (reduxState) => ({
+    reduxState
+})
+
+export default connect(putPropsOnRedux)(Review);
